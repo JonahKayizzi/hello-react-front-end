4382adcd70bd5f5ee22bfbf1164061736b7ed22d
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-23 22:14:56 +0300

    Modify index.js to render the App component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
