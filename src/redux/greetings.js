7ac50ae20fcc3562984bf7cd7afacb058056d23d
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-23 22:16:26 +0300

    Add a greeting redux store with reducer to fetch a random greeting from the rails API end point

---
+++ src/redux/greetings.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  greeting: null,
+};
+
+const URL = 'http://127.0.0.1:3000/api/v1/greetings';
+
+export const fetchGreeting = createAsyncThunk(
+  'greetings/fetchGreeting',
+  async () => {
+    const response = await axios.get(URL);
+    return response.data;
+  }
+);
+
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchGreeting.fulfilled]: (state, action) => {
+      state.greeting = action.payload;
+    },
+  },
+});
+
+export default greetingsSlice.reducer;
